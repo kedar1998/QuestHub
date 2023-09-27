1157d04aa601016425c99fb6d6004d7c3a604684
kedar1998 <kedarmakode1598@gmail.com>
2023-09-27 11:38:50 +0530

    Added custom ThemeProvider for light/dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,28 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
+import { ThemeProvider } from "@/context/ThemeProvider";
 
-export const metadata = {
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
   title: "QuestHub",
+  description: "Question answer platform",
+  // icons: {
+  //   icon: '/pulic/assets/icon.svg'
+  // },
 };
 
 export default function RootLayout({
@@ -11,9 +31,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <ThemeProvider>{children}</ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
