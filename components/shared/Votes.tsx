3ea897dd6ecd199b86408fe8a0830d944c650a1f
kedar1998 <kedarmakode1598@gmail.com>
2023-10-19 15:32:16 +0530

    Added voting functionality to question and answer

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,7 +1,157 @@
-import React from "react";
+"use client";
 
-const Votes = () => {
-  return <div>Votes</div>;
+import { downvoteAnswer, upvoteAnswer } from "@/lib/actions/answer.action";
+import {
+  downvoteQuestion,
+  upvoteQuestion,
+} from "@/lib/actions/question.action";
+import { formatAndDivideNumber } from "@/lib/utils";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+// import { useRouter } from "next/router";
+
+interface Props {
+  type: string;
+  itemId: string;
+  userId: string;
+  upvotes: number;
+  hasupVoted: boolean;
+  downvotes: number;
+  hasdownVoted: boolean;
+  hasSaved?: boolean;
+}
+
+const Votes = ({
+  type,
+  itemId,
+  userId,
+  upvotes,
+  hasupVoted,
+  downvotes,
+  hasdownVoted,
+  hasSaved,
+}: Props) => {
+  const pathname = usePathname();
+  // const router = useRouter();
+
+  const handleSave = () => {};
+
+  const handleVote = async (action: string) => {
+    if (!userId) {
+      return;
+    }
+
+    if (action === "upvote") {
+      if (type === "Question") {
+        await upvoteQuestion({
+          questionId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted,
+          hasdownVoted,
+          path: pathname,
+        });
+      } else if (type === "Answer") {
+        await upvoteAnswer({
+          answerId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted,
+          hasdownVoted,
+          path: pathname,
+        });
+      }
+
+      // TODO: show toast message
+    }
+
+    if (action === "downvote") {
+      if (type === "Question") {
+        await downvoteQuestion({
+          questionId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted,
+          hasdownVoted,
+          path: pathname,
+        });
+      } else if (type === "Answer") {
+        await downvoteAnswer({
+          answerId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted,
+          hasdownVoted,
+          path: pathname,
+        });
+      }
+
+      // TODO: show toast message
+    }
+  };
+
+  return (
+    <div className="flex gap-5">
+      <div className="flex-center gap-2.5">
+        {/* Upvotes */}
+        <div className="flex-center gap-1.5">
+          <Image
+            src={`${
+              hasupVoted
+                ? "/assets/icons/upvoted.svg"
+                : "/assets/icons/upvote.svg"
+            }`}
+            alt="upvote"
+            height={18}
+            width={18}
+            className="cursor-pointer"
+            onClick={() => handleVote("upvote")}
+          />
+
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatAndDivideNumber(upvotes)}
+            </p>
+          </div>
+        </div>
+
+        {/* Down Votes */}
+        <div className="flex-center gap-1.5">
+          <Image
+            src={`${
+              hasdownVoted
+                ? "/assets/icons/downvoted.svg"
+                : "/assets/icons/downvote.svg"
+            }`}
+            alt="downvote"
+            height={18}
+            width={18}
+            className="cursor-pointer"
+            onClick={() => handleVote("downvote")}
+          />
+
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatAndDivideNumber(downvotes)}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Save question to collection */}
+
+      {type === "Question" && (
+        <Image
+          src={`${
+            hasSaved
+              ? "/assets/icons/star-filled.svg"
+              : "/assets/icons/star-red.svg"
+          }`}
+          alt="star"
+          height={18}
+          width={18}
+          className="cursor-pointer"
+          onClick={handleSave}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Votes;
